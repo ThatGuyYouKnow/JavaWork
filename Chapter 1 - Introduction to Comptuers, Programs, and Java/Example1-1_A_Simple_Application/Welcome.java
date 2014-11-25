cb1e9ff055421742f25e15542e7b40503b5820ed
ThatGuyYouKnow <andrew.fleer@gmail.com>
2014-11-24 22:49:20 -0600

    Chapter one complete
    Finished chapter one.

---
+++ Chapter 1 - Introduction to Comptuers, Programs, and Java/Example1-1_A_Simple_Application/Welcome.java
@@ -0,0 +1,14 @@
+/**
+ * @author Fleer
+ * This application program prints Welcome to Java!
+ */
+public class Welcome {  // Welcome is the class name. Needs to match the file name!
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+	}
+}
+
+/*
+ * Every Java program must have at least one class
+ * Each class begins with a class declaration that defines data and methods for the class
+ */
---
+++ Chapter 1 - Introduction to Comptuers, Programs, and Java/Example1-2_Using_Message_Dialog_Boxes/WelcomeInMessageDialogBox.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+
+/**
+ * @author Fleer
+ * This application program displays Welcome to Java!
+ * in a message dialog box.
+ */
+public class WelcomeInMessageDialogBox {
+	public static void main(String[] args) {
+		// Display Welcome to Java! in a message dialog box
+		JOptionPane.showMessageDialog(null, "Welcome to Java!",
+				"Example 1.2 Output", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
