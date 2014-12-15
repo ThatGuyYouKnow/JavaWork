80f7f0e64019cb2cb0a29d3fc440c70f2b02496d
ThatGuyYouKnow <andrew.fleer@gmail.com>
2014-12-14 19:51:40 -0600

    Changed the program to use PI as a constant.

--- Chapter 2 - Primitive data Types and Operations/Example2-1_Computing_Area_for_a_Circle/ComputeArea.java
+++ Chapter 2 - Primitive data Types and Operations/Example2-1_Computing_Area_for_a_Circle/ComputeArea.java
@@ -9,6 +9,10 @@
 public class ComputeArea {
 	/** Main method */
 	public static void main(String[] args) {
+		// Declare PI to be a constant.
+		// Constant means it cannot change.
+		final double PI = 3.14159;
+		
 		double radius;
 		double area;
 		
@@ -16,7 +20,8 @@ public class ComputeArea {
 		radius = 20;
 		
 		// Compute area
-		area = radius * radius * 3.14159;
+		// This computation now uses the constant, PI.
+		area = radius * radius * PI;
 		
 		// Display results
 		System.out.println("The area for the circle of radius " + radius + " is " + area);
